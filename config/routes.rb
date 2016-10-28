a26bca34346abef9698f3470418c0c598d8b2b1d
Yerlan <bayekeshov@gmail.com>
2016-10-28 14:54:49 -0400

    Added 2 more pages:posts and upate

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  get '/' => 'welcome#index'
-  get 'users/profile' => 'welcome#profile'
-  get 'users/update' => 'welcome#update'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-get 'welcome/index', as: 'user_root'
+  devise_for :users
+  get '/' 									 => 'welcome#index'
+  get 'welcome/index', as: 'user_root'
+  get 'users/profile' 			 => 'welcome#profile'
+  get 'users/update'				 => 'welcome#update'
+	
+
+
+
+	get '/posts'           => 'posts#show'
 
 end
